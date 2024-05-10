db485ae7d6b3d4da9e931e22007c3cc638f645b0
wavybangsy <mary_erika_culala@dlsu.edu.ph>
2024-05-10 18:21:39 +0800

    extra mods + text alert

--- toSheet.js
+++ toSheet.js
@@ -17,6 +17,12 @@ const googleAuth = new google.auth.JWT(
   "https://www.googleapis.com/auth/spreadsheets"
 );
 
+/**
+ *
+ * @returns valuesFromSheet, all records of members' residency hours
+ * readSheet() function reads from 'TLS Residency Hours' tab in gsheet and returns the read rows,
+ * except header row.
+ */
 async function readSheet() {
   try {
     // google sheet instance
@@ -24,24 +30,28 @@ async function readSheet() {
       version: "v4",
       auth: googleAuth,
     });
+
     // read data in the range in a sheet
-    const infoObjectFromSheet = await sheetInstance.spreadsheets.values.get({
+    const dataFromSheet = await sheetInstance.spreadsheets.values.get({
       auth: googleAuth,
       spreadsheetId: googleSheetId,
       range: `${googleSheetPage}!A:B`,
     });
 
-    const valuesFromSheet = infoObjectFromSheet.data.values;
+    const valuesFromSheet = dataFromSheet.data.values;
 
-    // store values to array
     valuesFromSheet.shift(); // remove first row
-    let sheetData = sheetToArray(valuesFromSheet);
-    return sheetData;
+    return valuesFromSheet;
   } catch (err) {
     console.log("readSheet func() error", err);
   }
 }
 
+/**
+ *
+ * @returns valuesFromSheet, all logs of members ins and outs
+ * readLogs() functions reads from 'logs' locked tab in gsheet
+ */
 async function readLogs() {
   try {
     // google sheet instance
@@ -53,14 +63,11 @@ async function readLogs() {
     const infoObjectFromSheet = await sheetInstance.spreadsheets.values.get({
       auth: googleAuth,
       spreadsheetId: googleSheetId,
-      range: `logs!A:C`,
+      range: `logs!A:E`,
     });
 
     const valuesFromSheet = infoObjectFromSheet.data.values;
-
-    // store values to array
-    let sheetData = sheetToArray(valuesFromSheet);
-    return sheetData;
+    return valuesFromSheet;
   } catch (err) {
     console.log("readLogs func() error", err);
   }
@@ -91,18 +98,28 @@ function secondsToTime(seconds) {
   return time;
 }
 
-function storeDataToday(data, rows, uniqueValues) {
+/**
+ *TODO: time in, time out
+ */
+/**
+ *
+ * @param {*} data => empty array to fill up
+ * @param {*} logs => all logs record in logs tab
+ * @param {*} uniqueValues => list of members with residency hours today
+ * @returns data, an array of array with format: [member_name, time_in, time_out, total_time]
+ */
+function storeDataToday(data, logs, uniqueValues) {
   for (const value of uniqueValues) {
     let sum = 0;
 
-    for (const data of rows) {
-      // Convert currentDate to the same format as returned by getCurrentDate
-      let member = data[0];
-      let date = data[2];
+    for (const data of logs) {
+      var member = data[0];
+      let date = data[4];
+      let memberDuration = data[3];
 
       if (member == value && value != "" && date == dateToday) {
         //convert time to seconds
-        totalTimeInSeconds = timeToSeconds(data[1]);
+        totalTimeInSeconds = timeToSeconds(memberDuration);
         // Add to the sum in seconds
         sum += totalTimeInSeconds;
       }
@@ -110,56 +127,52 @@ function storeDataToday(data, rows, uniqueValues) {
 
     // Convert the sum back to "h m s" format
     sumTime = secondsToTime(sum);
-    console.log("Total Time:", sumTime);
+    console.log(`Total Time today of ${value}:`, sumTime);
 
-    // Update the sum in the sumData object
+    // add the member new record to data array
     data.push([value, sumTime]);
   }
   return data;
 }
 
-function sheetToArray(valuesFromSheet) {
-  if (valuesFromSheet) {
-    const dataList = [];
-
-    for (const row of valuesFromSheet) {
-      dataList.push(row);
-    }
-    return dataList;
-  } else {
-    console.log("Error pushing data to array.");
-    return [];
-  }
-}
-
-function updateRecord(todayData, sheet) {
+/**
+ *
+ * @param {*} todayData => array of members data with residency today
+ * @param {*} sheet => array of data from sheet
+ * queryRecord() function checks if the member has a record already in sheet, if not it creates a new record
+ * otherwise, it updates the member's record. (updateSheet function is called)
+ */
+async function queryRecord(todayData, sheet) {
   let newMembers = 0;
   for (const data of todayData) {
     let member = data[0];
     let memberFound = false;
 
-    console.log("Data: ", data);
-
     for (const [index, row] of sheet.entries()) {
       let sheetMember = row[0];
 
       if (member == sheetMember) {
-        console.log("Member found in row: ", row);
-        console.log("Member found in row index: ", index);
-        updateSheet(data, index + 1); // plus 1 for removing header row
+        console.log(`${member} previous record: `, row);
+        await updateSheet(data, index + 1, member); // plus 1 for removing header row
         memberFound = true;
         break;
       }
     }
     if (!memberFound) {
-      console.log("Add member to sheet");
+      console.log(`${member} successfully added to sheet`);
       updateSheet(data, sheet.length + 1 + newMembers); // plus 1 to jump to next empty cell in sheet
       newMembers += 1; // tracks actual sheet length while on the process of adding new members
     }
   }
 }
 
-async function updateSheet(row, index) {
+/**
+ *
+ * @param {*} row => data to put in sheet
+ * @param {*} index => row number in gsheet to update
+ * updateSheet() function updates the members' record in gsheet
+ */
+async function updateSheet(row, index, member) {
   try {
     // google sheet instance
     const sheetInstance = await google.sheets({
@@ -167,7 +180,7 @@ async function updateSheet(row, index) {
       auth: googleAuth,
     });
 
-    console.log("ROW TO PUT: ", row);
+    console.log(`${member} new record: `, row);
     index = index + 1; // plus 1 to follow sheet numbering
 
     // update data in the range
@@ -188,67 +201,69 @@ async function updateSheet(row, index) {
 module.exports = {
   savetoSheet: async function savetoSheet() {
     try {
-      // const csvData = fs.readFileSync("./logs.csv", "utf-8"); // Load CSV data from file
+      // gets all logs from 'logs' tab in sheet
+      let allLogs = await readLogs();
 
-      let logs = await readLogs();
-      console.log("from sheets: ", logs);
-
-      // Converting array of arrays to a format similar to CSV rows
-      const logsFormat = logs.map((row) => row.join(",")).join("\n");
+      // Converts array of arrays to a format similar to CSV rows
+      const logsFormat = allLogs.map((row) => row.join(",")).join("\n");
 
       // Splitting the CSV-like string back into rows and columns
-      const rows = logsFormat.split("\n").map((row) => row.split(",")); // CSV to aray
+      const logs = logsFormat.split("\n").map((row) => row.split(",")); // CSV-like string to aray
       dateToday = getCurrentDate().toString(); // get current date and turn to string to compare later
-      console.log(dateToday);
+      console.log("\nDate today: ", dateToday);
 
+      // get unique members from the logs today (duplicates removed if members has multiple logs in a day)
       const uniqueValues = [
         ...new Set(
-          rows
+          logs
             .filter(
               (row) =>
-                typeof row[2] !== "undefined" &&
-                row[2].trim() === dateToday.trim() &&
-                row[2].trim() !== ""
+                typeof row[4] !== "undefined" &&
+                row[4].trim() === dateToday.trim() &&
+                row[4].trim() !== ""
             )
             .map((row) => row[0])
         ),
       ];
 
-      console.log("Data: ", rows);
-
       let data = [];
 
       // store today's logs to array
-      data = storeDataToday(data, rows, uniqueValues);
+      data = storeDataToday(data, logs, uniqueValues);
 
       // print unique members with logs today
-      console.log("unique value:", uniqueValues);
+      console.log("Members today:", uniqueValues);
 
-      //print data to update in sheets
-      console.log("new data: ", data);
-
-      //read from sheet
+      //read from sheet (stored in an array)
       let sheetData = await readSheet();
-      console.log(sheetData);
 
-      //update sheet
-      updateRecord(data, sheetData);
-      return rows;
+      //check if record exists in sheet or not, then update sheet.
+      queryRecord(data, sheetData);
+      return logs;
     } catch (err) {
       console.log("error save to sheets", err);
       return [];
     }
   },
 
-  savetoLogs: async function saveToLogs(user, time, today, callback) {
+  savetoLogs: async function saveToLogs(
+    user,
+    timein,
+    timeout,
+    time,
+    today,
+    callback
+  ) {
     try {
       let logs = await readLogs();
-      let logs_length = logs.length;
-      let index = logs_length + 1;
-      let row = [user, time, today];
+      var index = 1;
 
-      console.log("row read: ", logs_length);
-      // console.log("row to save: ", row);
+      if (logs != undefined) {
+        let logs_length = logs.length;
+        index = logs_length + 1;
+      }
+
+      let row = [user, timein, timeout, time, today];
 
       // google sheet instance
       const sheetInstance = await google.sheets({
@@ -259,7 +274,7 @@ module.exports = {
       await sheetInstance.spreadsheets.values.update({
         auth: googleAuth,
         spreadsheetId: googleSheetId,
-        range: `logs!A${index}:C`,
+        range: `logs!A${index}:E`,
         valueInputOption: "RAW",
         resource: {
           values: [row],
